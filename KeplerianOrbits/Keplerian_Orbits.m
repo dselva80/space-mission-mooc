7e6f74b39a2e50e0f6276f30c8ec91e93be21b13
PatrickVoorhees <pwv9@cornell.edu>
2016-07-11 11:48:59 -0400

    Changing Keplerian_Orbits
    Faster run because the background is no longer a saved image

--- KeplerianOrbits/Keplerian_Orbits.m
+++ KeplerianOrbits/Keplerian_Orbits.m
@@ -1,13 +1,31 @@
+% find euler angles, postrotatefun -- help from Dmitry ??
+
 function Keplerian_Orbits
-% set options, open a figure and set the axes
+% Function KEPLERIAN_ORBITS allows for user alteration of several
+% orbital parameters and animates the orbit(s) when selected by the user.
+%
+% Written by Patrick Voorhees 11-Jul-2016 11:23:00
+%       Copyright (c) 2016 Patrick Voorhees (pwv9@cornell.edu)
+% Advisors: Professors Dmitry Savransky and Daniel Selva (Cornell 
+%       University, Department of Mechanical and Aerospace Engineering)
+% Purpose: create a visualization of the effects of orbital parameters on
+%       the shape, size, and orientation of an orbit, as well as on the 
+%       velocity, position, and period of an orbiting body 
+%       (MAE 4060: Introduction to Spaceflight Mechanics). 
+% NOTE: two orbits may be selected to demonstrate phenomena such as
+%       resonance, however each orbiting body does not effect the other, 
+%       this is solved as a strictly two body problem.
+
+% set options for figure display, open a figure and set the axes
 ops = {'BackgroundColor','ForegroundColor','FontWeight','FontSize'};
-H.fig = figure('Name','KeplerianOrbits','Position',[120 60 900 600],'Color','k');
-H.ax = axes('Units','Pixels','Position',[0 50 600 600],...
-    'xcolor','k','ycolor','k','zcolor','k','Color','k');
-H.text14 = uicontrol('Style','text','Position',[20 10 560 30],'String','',...
-    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
-H.text15 = uicontrol('Style','text','Position',[20 50 560 30],'String','',...
-    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
+H.fig = figure('Name','Keplerian_Orbits','Position',[320 60 900 600],'Color','k','Resize','off');
+H.ax1 = axes('Units','Pixels','Position',[0 50 600 600],...
+    'xcolor','none','ycolor','none','zcolor','none','Color','none');
+uistack(H.ax1,'bottom');
+H.text14 = uicontrol('Style','text','Position',[20 10 500 30],'String','',...
+    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10,'HorizontalAlignment','left');
+H.text15 = uicontrol('Style','text','Position',[20 50 500 30],'String','',...
+    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10,'HorizontalAlignment','left');
 
 % set first orbit options
 H.text2 = uicontrol('Style','text','Position',[625 575 100 20],'String','FIRST ORBIT',...
@@ -21,25 +39,25 @@ H.edit1 = uicontrol('Style','edit','Position',[625 490 100 25],'String','.723',.
 H.text4 = uicontrol('Style','text','Position',[600 460 150 20],'String','e1 = [0,.9]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider2 = uicontrol('Style','slider','Position',[615 425 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',.9,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',.9,'Value',0,'SliderStep',[1/30 1/9]);
 H.edit2 = uicontrol('Style','edit','Position',[625 395 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text5 = uicontrol('Style','text','Position',[600 360 150 20],'String','i1 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider3 = uicontrol('Style','slider','Position',[615 330 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit3 = uicontrol('Style','edit','Position',[625 300 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text6 = uicontrol('Style','text','Position',[600 265 150 20],'String','RAAN1 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider4 = uicontrol('Style','slider','Position',[615 235 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit4 = uicontrol('Style','edit','Position',[625 205 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text7 = uicontrol('Style','text','Position',[600 175 150 20],'String','w1 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider5 = uicontrol('Style','slider','Position',[615 145 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit5 = uicontrol('Style','edit','Position',[625 115 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 
@@ -55,37 +73,62 @@ H.edit6 = uicontrol('Style','edit','Position',[775 490 100 25],'String','.723',.
 H.text10 = uicontrol('Style','text','Position',[750 460 150 20],'String','e2 = [0,.9]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider7 = uicontrol('Style','slider','Position',[765 425 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',.9,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',.9,'Value',0,'SliderStep',[1/30 1/9]);
 H.edit7 = uicontrol('Style','edit','Position',[775 395 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text11 = uicontrol('Style','text','Position',[750 360 150 20],'String','i2 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider8 = uicontrol('Style','slider','Position',[765 330 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit8 = uicontrol('Style','edit','Position',[775 300 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text12 = uicontrol('Style','text','Position',[750 265 150 20],'String','RAAN2 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider9 = uicontrol('Style','slider','Position',[765 235 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit9 = uicontrol('Style','edit','Position',[775 205 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.text13 = uicontrol('Style','text','Position',[750 175 150 20],'String','w2 = [0,360]',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 H.slider10 = uicontrol('Style','slider','Position',[765 145 120 30],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0);
+    'Bold',ops{4},10,'Min',0,'Max',360,'Value',0,'SliderStep',[1/72 1/12]);
 H.edit10 = uicontrol('Style','edit','Position',[775 115 100 25],'String','0',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
 
 % set animation preferences
-H.orbits = uicontrol('Style','checkbox','Position',[625 60 100 25],'String','2 Orbits?',...
+H.orbits = uicontrol('Style','checkbox','Position',[500 60 100 25],'String','2 Orbits?',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
-H.anim = uicontrol('Style','togglebutton','Position',[625 20 100 25],'String','Play',...
+H.anim = uicontrol('Style','togglebutton','Position',[500 20 100 25],'String','Play',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
-H.pref = uicontrol('Style','popupmenu','Position',[775 60 100 25],'String',{'Rotate on','Zoom on'},...
+H.text16 = uicontrol('Style','text','Position',[612 60 126 20],'String','Manual Commands',...
     ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
-H.speed = uicontrol('Style','slider','Position',[765 20 120 25],ops{1},'k',ops{2},'w',ops{3},...
-    'Bold',ops{4},10,'Min',.5,'Max',4,'Value',2);
+H.pref = uicontrol('Style','popupmenu','Position',[625 20 100 25],'String',{'Rotate on','Zoom on'},...
+    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
+H.text17 = uicontrol('Style','text','Position',[765 60 120 20],'String','Animation Speed',...
+    ops{1},'k',ops{2},'w',ops{3},'Bold',ops{4},10);
+H.speed = uicontrol('Style','slider','Position',[775 20 100 25],ops{1},'k',ops{2},'w',ops{3},...
+    'Bold',ops{4},10,'Min',.25,'Max',3,'Value',.25);
+
+% set background image to simulate space
+Nstars = 400;
+xpos = 2.9*rand(1,Nstars)-1.2;
+ypos = 2.9*rand(1,Nstars)-1.2;
+siz = .011*rand(1,Nstars);
+th = linspace(0,2*pi,50);
+hold on;
+for i=1:Nstars
+    % check if background stars are too small to be visible, resize, plot
+    if siz(i) <= .001
+        siz(i) = 10*siz(i);
+    end
+    fill(siz(i)*cos(th)+xpos(i),siz(i)*sin(th)+ypos(i),'w');
+end
+hold off;
+
+% set second set of axes for the plotting over background image
+H.ax2 = axes('Units','Pixels','Position',[0 50 600 600],...
+    'xcolor','none','ycolor','none','zcolor','none','Color','none');
+uistack(H.ax2,'top');
 
 % set callback functions
 set(H.slider1,'Callback',{@sliderCB,H});
@@ -113,38 +156,12 @@ set(H.anim,'CallBack',{@animateOrbit,H});
 set(H.pref,'Callback',{@animateOrbit,H});
 set(H.speed,'Callback',{@animateOrbit,H});
 
-% set background as space
-xpos = 2.9*rand(1,400)-1.2;
-ypos = 2.9*rand(1,400)-1.2;
-siz = .011*rand(1,400);
-th = linspace(0,2*pi,50);
-hold on;
-for i=1:length(siz)
-    % check if background stars are too small to be visible, resize, plot
-    if siz(i) <= .001
-        siz(i) = 10*siz(i);
-    end
-    fill(siz(i)*cos(th)+xpos(i),siz(i)*sin(th)+ypos(i),[.1 .1 .1]);
-end
-
-% set background color to black and fill the stars (circles) with white
-set(gca,'Color','w');
-set(gca,'xcolor','w');
-set(gca,'ycolor','w');
-set(gca,'zcolor','w');
-set(gcf,'Color','w');
-
-% save figure as image, produce negative and set as background for figure
-saveas(H.ax,'space','jpg');
-neg('space.jpg');
-ax = axes('units','normalized','position',[0 0 1 1]);
-uistack(ax,'bottom');
-im = image(imread('space.jpg'));
-set(ax,'handlevisibility','off');
-set(im,'alphadata',1);
-hold off;
+% plot initial orbit
+animateOrbit(0,0,H);
 
 function sliderCB(~,~,H)
+% callback function for each slider, just resets the edit text strings to
+% match the values of the sliders
 set(H.edit1,'String',get(H.slider1,'Value'));
 set(H.edit2,'String',get(H.slider2,'Value'));
 set(H.edit3,'String',get(H.slider3,'Value'));
@@ -158,6 +175,9 @@ set(H.edit10,'String',get(H.slider10,'Value'));
 animateOrbit(0,0,H);
 
 function editCB(~,~,H)
+% callback function for each edit text, just resets the slider values to
+% match the values of the edit text strings (must check that the value is
+% within the correct bounds of the slider so it does not cause an error)
 set(H.slider1,'Value',checkBound(str2double(get(H.edit1,'String')),.723,1.523));
 set(H.slider2,'Value',checkBound(str2double(get(H.edit2,'String')),0,.9));
 set(H.slider3,'Value',checkAngle(str2double(get(H.edit3,'String')),0,360));
@@ -171,6 +191,7 @@ set(H.slider10,'Value',checkAngle(str2double(get(H.edit10,'String')),0,360));
 animateOrbit(0,0,H);
 
 function [a1,a2,e1,e2,i1,i2,R1,R2,w1,w2,sp] = read(H)
+% reads in all the necessary data once for use in other functions
 a1 = get(H.slider1,'Value');
 e1 = get(H.slider2,'Value');
 i1 = get(H.slider3,'Value')*pi/180;
@@ -185,9 +206,7 @@ sp = get(H.speed,'Value');
 
 function [x,y,z,t] = calculateOrbit(a,e,i,R,w)
 % This function will find the time and Cartesian coordinates of the orbits.
-% Initialize angle and anomaly, set standard gravitational parameter of 
-% the sun, and read in all of the necessary data.
-mu = .000295913; % au^3 / day^2          
+mu = .000295913; % au^3 / day^2    standard gravitational parameter of sun
 M = linspace(0,2*pi*sqrt((a^3)/mu),100);
 E = M;
 
@@ -206,11 +225,12 @@ if e > 0
         num = num+1;
     end
 end
+% compute time from eccentric anomaly
 t = (E/sqrt((a^3)/mu)-e*sin(E/sqrt((a^3)/mu)))/sqrt(mu/((a)^3));
 
 % Before calculating the direction cosine matrix for the orbit(s), it is
-% necessary to check if the orbit is inclined or not because the RAAN is
-% undefined for non-inclined orbits.
+% necessary to check if the orbit is inclined or not because the right 
+% ascension of the ascending node is undefined for non-inclined orbits.
 if i == 0
     R = 0;
 end
@@ -220,11 +240,16 @@ dcm = [cos(R)*cos(w)*cos(i)-sin(R)*sin(w),-cos(R)*cos(i)*sin(w)-sin(R)*cos(w),-c
     sin(R)*cos(w)*cos(i)+cos(R)*sin(w),-sin(R)*sin(w)*cos(i)+cos(R)*cos(w),-sin(R)*sin(i);...
     sin(i)*cos(w),-sin(i)*sin(w),cos(i)];
 
-% calculate first orbit using the parameters, from body frame into inertial
+% calculate orbit using the parameters, from body frame into inertial
+% note that b3 = 0 since the body frame of an orbit is located in the plane
+% of that orbit
+% also x and y are negative for aesthetic purposes only, to orient the
+% orbit in such a way so apoapse is nearest to the user and so the body
+% orbits counter clockwise as viewed from above in a prograde orbit
 b1 = a*cos(2*pi*t/t(end))+a*e^2;
 b2 = a*sqrt(1-e^2)*sin(2*pi*t/t(end));
-x = dcm(1,1)*b1+dcm(1,2)*b2;
-y = dcm(2,1)*b1+dcm(2,2)*b2;
+x = -(dcm(1,1)*b1+dcm(1,2)*b2);
+y = -(dcm(2,1)*b1+dcm(2,2)*b2);
 z = dcm(3,1)*b1+dcm(3,2)*b2;
 
 function animateOrbit(~,~,H)
@@ -244,24 +269,19 @@ end
 
 % if two orbits are preferred by the user then plot two, else only one
 if get(H.orbits,'Value') == 1
-    plot3(x1,y1,z1,'--w',x2,y2,z2,'--w','LineWidth',1.5);
+    plot3(H.ax2,x1,y1,z1,'--w',x2,y2,z2,'--w','LineWidth',1.5);
 else
-    plot3(x1,y1,z1,'--w','LineWidth',1.5);
+    plot3(H.ax2,x1,y1,z1,'--w','LineWidth',1.5);
 end
 hold on;
-% set plot color to transparent so the space background is visible
-set(gca,'xcolor','None');
-set(gca,'ycolor','None');
-set(gca,'zcolor','None');
-set(gca,'Color','None');
-set(gcf,'Color','None');
+set(H.ax2,'xcolor','none','ycolor','none','zcolor','none','Color','none')
 
 % Create a set of axes, set the axes equal, and make the axis labels invisible.
 % This will enhance visual aid by providing lines of reference.
 ax = -get(H.slider1,'Max')*(1.1+get(H.slider2,'Max')):get(H.slider1,'Max')*(1.1+get(H.slider2,'Max'));
-plot3(ax,zeros(1,length(ax)),zeros(1,length(ax)),'r','LineWidth',2);
-plot3(zeros(1,length(ax)),ax,zeros(1,length(ax)),'g','LineWidth',2);
-plot3(zeros(1,length(ax)),zeros(1,length(ax)),ax,'b','LineWidth',2);
+plot3(H.ax2,ax,zeros(1,length(ax)),zeros(1,length(ax)),'r','LineWidth',2);
+plot3(H.ax2,zeros(1,length(ax)),ax,zeros(1,length(ax)),'g','LineWidth',2);
+plot3(H.ax2,zeros(1,length(ax)),zeros(1,length(ax)),ax,'b','LineWidth',2);
 axis equal;
 if get(H.pref,'Value') == 1
     rotate3d on;
@@ -275,41 +295,45 @@ max_a2 = get(H.slider6,'Max');
 
 % time index
 ind1 = round(length(t1)/2); 
-ind2 = round(length(t2)/2);
+ind2 = ind1;
 
 % create the data for the orbiting bodies and sun (centered at the origin)
 [x,y,z] = sphere;
-sun = fill3(max_a1*x/12,max_a1*y/12,max_a1*z/12,'y');
-set(sun,'EdgeColor','y','FaceColor','y','LineWidth',3);
-planet1 = fill3(max_a1*x/15+x1(ind1),max_a1*y/15+y1(ind1),max_a1*z/15+z1(ind1),'g');
-set(planet1,'EdgeColor','g','FaceColor','g','LineWidth',2);
+surf(H.ax2,max_a1*x/12,max_a1*y/12,max_a1*z/12,'EdgeColor','none','FaceColor','y');
+planet1 = surf(H.ax2,max_a1*x/15+x1(ind1),max_a1*y/15+y1(ind1),max_a1*z/15+z1(ind1),...
+    'EdgeColor','none','FaceColor','g');
 if get(H.orbits,'Value') == 1
-    planet2 = fill3(max_a2*x/15+x2(ind2),max_a2*y/15+y2(ind2),max_a2*z/15+z2(ind2),'r');
-    set(planet2,'EdgeColor','r','FaceColor','r','LineWidth',2);
+    planet2 = surf(H.ax2,max_a2*x/15+x2(ind2),max_a2*y/15+y2(ind2),max_a2*z/15+z2(ind2),...
+        'EdgeColor','none','FaceColor','r');
 end
 hold off;
 
+% reset the string of the toggle button depending on its current state
+if get(H.anim,'Value') == 1
+    set(H.anim,'String','Pause');
+else
+    set(H.anim,'String','Play');
+end
+
 % Update the data if the the user preference is set to play animation. 
-% Continue animation as long as it is selected but no longer than 10
-% periods for either orbit. 
-periods1 = 0; periods2 = 0;
-while (get(H.anim,'Value') == 1) && (periods1 < 10) && (periods2 < 10)
+% Continue animation as long as it is selected
+while get(H.anim,'Value') == 1
     % update time but check that it is in the appropriate range
-    ind1 = ind1 + sp*2*n(1);
+    ind1 = ind1 + sp*ceil(n(1));
     if ind1 > length(x1)
         ind1 = ind1 - length(x1);
-        periods1 = periods1 + 1;
     end
-    ind2 = ind2 + sp*2*n(2);
+    ind2 = ind2 + sp*ceil(n(2));
     if ind2 > length(x2)
         ind2 = ind2 - length(x2);
-        periods2 = periods2 + 1;
     end
 
     % update position of the planets depending on the updated time
-    set(planet1,'XData',max_a1*x/15+x1(ceil(ind1)),'YData',max_a1*y/15+y1(ceil(ind1)),'ZData',max_a1*z/15+z1(ceil(ind1)));
+    set(planet1,'XData',max_a1*x/15+x1(ceil(ind1)),'YData',max_a1*y/15+y1(ceil(ind1)),...
+        'ZData',max_a1*z/15+z1(ceil(ind1)));
     if get(H.orbits,'Value') == 1 && get(H.anim,'Value') == 1
-        set(planet2,'XData',max_a2*x/15+x2(ceil(ind2)),'YData',max_a2*y/15+y2(ceil(ind2)),'ZData',max_a2*z/15+z2(ceil(ind2)));
+        set(planet2,'XData',max_a2*x/15+x2(ceil(ind2)),'YData',max_a2*y/15+y2(ceil(ind2)),...
+            'ZData',max_a2*z/15+z2(ceil(ind2)));
     end
     drawnow
     pause(eps);
@@ -318,20 +342,15 @@ while (get(H.anim,'Value') == 1) && (periods1 < 10) && (periods2 < 10)
     % understanding of the effects of the orbital parameters
     r1 = sqrt(x1(ceil(ind1))^2+y1(ceil(ind1))^2+z1(ceil(ind1))^2);
     v1 = sqrt(mu*(2/r1-1/a1));
-    T1 = t1(end);
     r2 = sqrt(x2(ceil(ind2))^2+y2(ceil(ind2))^2+z2(ceil(ind2))^2);
     v2 = sqrt(mu*(2/r2-1/a2));
-    T2 = t2(end);
-    s1 = sprintf('r1 = %2.4f au, v1 = %2.4f au/day, T1 = %2.2f days\n',r1,v1,T1);
-    s2 = sprintf('r2 = %2.4f au, v2 = %2.4f au/day, T2 = %2.2f days\n',r2,v2,T2);
+    s1 = sprintf('r1 = %2.4f au, v1 = %2.4f au/day, T1 = %2.2f days\n',r1,v1,t1(end));
+    s2 = sprintf('r2 = %2.4f au, v2 = %2.4f au/day, T2 = %2.2f days\n',r2,v2,t2(end));
     if get(H.orbits,'Value') == 1
         set(H.text15,'String',[s1 s2]);
     else
         set(H.text15,'String',s1);
     end
-end
-if periods1 >= 10 || periods2 >= 10
-    set(H.anim,'Value',0,'String','Play');
 end
 
 % print values
@@ -340,13 +359,11 @@ printParameters(H);
 function printParameters(H)
 % read in data and print to the figure after every update in the data
 [a1,a2,e1,e2,i1,i2,R1,R2,w1,w2,~] = read(H);
-s1=sprintf('a1 = %2.3f au, e1 = %2.2f, i1 = %2.2f deg, RAAN1 = %2.2f deg, w1 = %2.2f deg\n',a1,e1,i1,R1,w1);
+s = sprintf('a1 = %2.3f au, e1 = %2.2f, i1 = %2.2f deg, RAAN1 = %2.2f deg, w1 = %2.2f deg\n',a1,e1,i1,R1,w1);
 if get(H.orbits,'Value') == 1    
-    s2= sprintf('a2 = %2.3f au, e2 = %2.2f, i2 = %2.2f deg, RAAN2 = %2.2f deg, w2 = %2.2f deg',a2,e2,i2,R2,w2);
-    set(H.text14,'String',[s1 s2],'FontWeight','Bold','FontSize',10,'BackgroundColor','k','ForegroundColor','w');
-else
-    set(H.text14,'String',s1,'FontWeight','Bold','FontSize',10,'BackgroundColor','k','ForegroundColor','w');
+    s = [s sprintf('a2 = %2.3f au, e2 = %2.2f, i2 = %2.2f deg, RAAN2 = %2.2f deg, w2 = %2.2f deg',a2,e2,i2,R2,w2)];
 end
+set(H.text14,'String',s,'FontWeight','Bold','FontSize',10,'BackgroundColor','k','ForegroundColor','w');
 
 function var = checkBound(var,low,high)
 % checks to make sure that the slider value is within the appropriate range
@@ -361,21 +378,8 @@ end
 function var = checkAngle(var,low,high)
 % checks to make sure that the angle value is within the appropriate range
 while var < low
-    var = var + high - low;
+    var = var + (high - low);
 end
 while var > high
-    var = var - high + low;
+    var = var - (high - low);
 end
-
-function neg(filename)
-% function takes an image, reads the data and produces a negative version
-I = imread(filename);
-[a,b,c] = size(I);
-for i = 1:a
-    for j = 1:b
-        for k = 1:c
-            I(i,j,k) = 255-I(i,j,k);
-        end
-    end
-end
-imwrite(I,filename);
