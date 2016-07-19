383a446fb0d84ef3b1bd7c09ffb2e2f51f7c4b23
allegraem <aem285@cornell.edu>
2016-07-19 19:51:30 -0400

    updated ground track gui
    made loop faster by getting rid of call to axes
    changed the way swath is plotted. its now an azimuthal projection
    rather than a doctored cylindrical projection
    no longer need function flat_swath

--- GrountrackGUI/flat_swath.m
+++ GrountrackGUI/flat_swath.m
@@ -1,4 +1,4 @@
-function [ A, mincol, maxcol, minrow, maxrow ] = flat_swath( circ_rows,circ_cols,latg,r,height,width )
+function [ A, mincol, maxcol, minrow, maxrow ] = flat_swath( circ_rows,circ_cols,latg,r,height,width,indec )
 % OUTPUTS
 % A where A is an image
 
@@ -11,6 +11,7 @@ quart = round(length(circ_rows)/4);
 half = round(length(circ_cols)/2);
 tf = quart+half;
 
+
 % this is the basecase (but useful to put it here)
 minrow = min(circ_rows);
 maxrow = max(circ_rows);
@@ -19,57 +20,83 @@ maxcol = max(circ_cols);
 
 
 if latg+r >= 90
+    disp('north pole')
     % at north pole
     % have a circle of rows and cols that cannot be fully displayed
     % add another flipped image to top of map so that the edges aren't a
     % problem.
     
+   
     
     % have to split up the circle in half to add a displacement to only
     % half
-    bottom_rows = circ_rows(quart+1:tf);
-    top_rows = cat(1,circ_rows(1:quart),circ_rows(tf+1:end));
-    top_rows=top_rows+(height-top_rows).*2;
-
     
-    % This is where I fudged it. It's not too noticeable at small (<50 FOV)
-    % if cols are not fudged the map tries to display all cols and it looks
-    % bad
+    % bottom rows is where x is neg
+    bottom_rows = circ_rows(indec);
+    circ_rows(indec)=400;
+    top_rows = circ_rows(circ_rows~=400);
+%     top_rows=height+(height-min(top_rows));
+    maxrow = height+min(top_rows);
+    minrow = min(circ_rows);
     
-    minrow=min(bottom_rows);
-    maxrow=max(top_rows);
     
-    mincol=.4*width;
-    maxcol=mincol+maxrow-minrow;
 
+    if maxcol-mincol > width-20
+        disp('both')
+        beg_cols = zeros(size(circ_cols));
+        end_cols = zeros(size(circ_cols));
+        % split circular cols into two different arrays
+        for q = 1:length(circ_cols)
+            if circ_cols(q) < 180
+                beg_cols(q) = circ_cols(q);
+            else
+                end_cols(q) = circ_cols(q);
+            end
+        end
+        beg_cols=beg_cols(beg_cols~=0)+width;
+        end_cols=end_cols(end_cols~=0);
 
-    im = cat(1,topo,fliplr(flip(topo)));
+        maxcol = max(beg_cols);
+        mincol = min(end_cols);
+    end
+    
+    
+    
+    
+%     if r>25
+%         disp('r')
+%         maxrow=height;
+%     end
+  
+
+    im1 = cat(1,topo,rot90(topo,-2));
+    im = cat(2,im1,im1);
 
     A = im(minrow:maxrow,mincol:maxcol);
 
 
  elseif latg-r<=-90
     % at south pole
-     bottom_rows = circ_rows(quart+1:tf);
-    top_rows = cat(1,circ_rows(1:quart),circ_rows(tf+1:end));
+    disp('south pole')
+    top_rows = circ_rows(indec);
+    circ_rows(indec) = 400;
+    bottom_rows = circ_rows(circ_rows~=400);
     bottom_rows = bottom_rows+height;
-    top_rows = top_rows+height;
+    top_rows = height-top_rows;
 
     
-    
-    mincol=round(width*.4);
-    maxcol=round(width*.55);
     minrow=min(bottom_rows);
     maxrow=max(top_rows);
 
+    
 
-    im = cat(1,flip(topo,1),fliplr(topo));
-
+    im1 = cat(1,fliplr(flip(topo)),topo);
+    im = cat(2,im1,im1);
     A = im(minrow:maxrow,mincol:maxcol);
     
- elseif maxcol-mincol > width-10
+ elseif maxcol-mincol > width-20
     % at prime meridian must loop longitude
-
+    disp('prime')
     beg_cols = zeros(size(circ_cols));
     end_cols = zeros(size(circ_cols));
     % split circular cols into two different arrays
@@ -86,7 +113,7 @@ if latg+r >= 90
     maxcol = max(beg_cols);
     mincol = min(end_cols);
 
-    im = cat(2,(topo),(topo));
+    im = cat(2,topo,topo);
     A = im(minrow:maxrow,mincol:maxcol);
 
 else
